7e57965d926060150b35ec85fa078daf877bba7a
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-06-19 13:03:20 -0500

    initial commit for master branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the React and the ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 // Create a react component
 const App = () => {
@@ -8,15 +9,44 @@ const App = () => {
     const labelText = 'Enter name:';
 
     return (
-        <div className="test">
-            <label className="label" htmlFor="name">{labelText}</label>
-            <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        <div>
+            <div className="container">
+                <h2>Learning React Repo Layout</h2>
+                <p>Every branch is a different exercise or application</p>
+                    <ol>
+                        <li>
+                            <span className='list-header'>jsx-ex</span>
+                            <ul>
+                                <li>Intro to JSX</li>
+                                <li>How to call variables / syntax</li>
+                            </ul>
+                        </li>
+                        <li>
+                        <span className='list-header'>components-ex</span>
+                        <ul>
+                            <li>Intro to react components</li>
+                            <li>Creation and implementation in other files</li>
+                        </ul>
+                    </li>
+                        <li>
+                            <span className='list-header'>seasons</span>
+                            <ul>
+                                <li>Small app that shows 2 weather options dependent on user's geolocation</li>
+                            </ul>
+                        </li>
+                    </ol>
+            </div>
+            <div>
+                <h3>Testing how to call variables in React</h3>
+                <label className="label" htmlFor="name">{labelText}</label>
+                <input id="name" type="text"/>
+                <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+            </div>
         </div>
     );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.querySelector('#root')
 );
