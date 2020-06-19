827eaa6930b397743604525ce096b0683a8dd5ab
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-06-19 12:37:11 -0500

    learning about components, creation/import/export

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from "./SeasonDisplay";
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-
-class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>
-    }
-}
+const App = () => {
+    return (
+        <div className={'ui container comments'}>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning! </h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam"
+                               timeAgo="Today at 4:00PM"
+                               avatar={faker.image.avatar()}
+                               commentText="Lorem ipsum dolor."/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex"
+                               timeAgo="Today at 2:00AM"
+                               avatar={faker.image.avatar()}
+                               commentText="Thursday! Whoo!!"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane"
+                               timeAgo="Yesterday at 4:50PM"
+                               avatar={faker.image.avatar()}
+                               commentText="Well my dog died today...left me quite alone."/>
+            </ApprovalCard>
+        </div>
+    );
+};
 
 ReactDOM.render(
     <App/>,
