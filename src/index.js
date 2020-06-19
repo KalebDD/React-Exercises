3affd2d48a26a092262a5298c102b75cf0eb0261
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-06-19 13:04:45 -0500

    learning about jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Import the React and the ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
 
 // Create a react component
 const App = () => {
@@ -10,32 +9,6 @@ const App = () => {
 
     return (
         <div>
-            <div className="container">
-                <h2>Learning React Repo Layout</h2>
-                <p>Every branch is a different exercise or application</p>
-                    <ol>
-                        <li>
-                            <span className='list-header'>jsx-ex</span>
-                            <ul>
-                                <li>Intro to JSX</li>
-                                <li>How to call variables / syntax</li>
-                            </ul>
-                        </li>
-                        <li>
-                        <span className='list-header'>components-ex</span>
-                        <ul>
-                            <li>Intro to react components</li>
-                            <li>Creation and implementation in other files</li>
-                        </ul>
-                    </li>
-                        <li>
-                            <span className='list-header'>seasons</span>
-                            <ul>
-                                <li>Small app that shows 2 weather options dependent on user's geolocation</li>
-                            </ul>
-                        </li>
-                    </ol>
-            </div>
             <div>
                 <h3>Testing how to call variables in React</h3>
                 <label className="label" htmlFor="name">{labelText}</label>
